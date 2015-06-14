73d3c066011f1313f2e2f7507a1c678fca16375d
jorgepp <cuanto.p.p@gmail.com>
2015-06-14 07:23:50 -0300

    para tomar tiempos

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,34 @@
 
 using namespace std;
 
+timeval start, endTime;
+//////////////////////////////////////////////////////////////
+void init_time()
+{
+      gettimeofday(&start,NULL);
+}
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+double get_time()
+{
+      gettimeofday(&endTime,NULL);
+      return
+(1000000*(endTime.tv_sec-start.tv_sec)+(endTime.tv_usec-start.tv_usec))/1000000.0;
+}
+//////////////////////////////////////////////////////////////
+
 int main(int argc, char *argv[])
 {
 //	if(argc != 6) { //archivo_input ; k; metodo, bloque (solo para metodo 3), submetodo (solo para metodo 0)
 //		usage();
 //		return 0;
 //	}
+
+
+
+    init_time();
+
 
 	string matlabCommand1 = "nohup matlab -nodisplay -nosplash -nojvm -r \"Image2Csv('";
 	matlabCommand1 += argv[1];
@@ -351,6 +373,8 @@ int main(int argc, char *argv[])
 		default:
             throw runtime_error("No existe ese metodo");
     }
+
+    cout << "Tiempo de computo "<<get_time()<<endl;
     return  0;
 
 }
