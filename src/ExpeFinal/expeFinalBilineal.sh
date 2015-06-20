a2ecc8820b18cb9543ed4f6c0de1ca5a3288702a
jarmagno <jarmagno@dc.uba.ar>
2015-06-20 04:22:16 -0300

    psnr expe final bilineal, subo el script, y las imagenes, para ver despues quien toma los tiempos de todo expeFInalBilineal.ods

---
+++ src/ExpeFinal/expeFinalBilineal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#./main imagen6.tiff_REDUCIDA1.tiff 1 1
+#./main imagen6.tiff_REDUCIDA2.tiff 2 1
+#./main imagen6.tiff_REDUCIDA4.tiff 4 1
+./main Lago.tiff_REDUCIDA1.tiff 1 1
+./main Lago.tiff_REDUCIDA3.tiff 3 1
+./main Lago.tiff_REDUCIDA5.tiff 5 1
+./main Piano.tiff_REDUCIDA1.tiff 1 1
+./main Piano.tiff_REDUCIDA2.tiff 2 1
+./main Piano.tiff_REDUCIDA3.tiff 3 1
+./main venus.tiff.gris.tiff_REDUCIDA1.tiff 1 1
+./main venus.tiff.gris.tiff_REDUCIDA3.tiff 3 1
+./main venus.tiff.gris.tiff_REDUCIDA7.tiff 7 1
